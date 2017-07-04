53ba9f2fb3bb8919c0c992d4a29f8fdec0f81cd7
prathameshs <prathamesh.s@vmax.com>
2017-07-04 13:49:36 +0530

    3.7.0 update

--- app/src/main/java/com/vmax/demo/banner/MainActivity.java
+++ app/src/main/java/com/vmax/demo/banner/MainActivity.java
@@ -1,12 +1,9 @@
 package com.vmax.demo.banner;
 
 
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.vmax.android.ads.api.VmaxAdView;
-
-
 
 
 /**Its Recommended To Use VMAX plugin For Android Studio To Add Your Dependencies
@@ -16,73 +13,23 @@ import com.vmax.android.ads.api.VmaxAdView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public VmaxAdView vmaxAdView;
+
+    VmaxAdView vmaxAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vmaxAdView=(VmaxAdView)findViewById(R.id.banner_adview);
 
-        loadBanner();
-    }
-
-    /** Method for adding Banner Through XML */
-    public void loadBanner()
-    {
-        /** Container for Ad*/
-         vmaxAdView = (VmaxAdView) findViewById(R.id.banner_adview);
-
-        /** To Fetch Your AdvId you can check your device's Google settings under ads subMenu Or You can Run this app Once and check
-         * the logs for 'AdRequested with url' under the tag vmax, from the url your Advid
-         * would be one of the parameters in the post request eg. advid=2cf626f0-08ac-4a4d-933c-00ecd0256cf4*/
-
-        /** DON'T INCLUDE vmaxAdView.setTestDevices() WHILE GOING LIVE WITH YOUR PROJECT AS THIS SERVES ONLY TEST ADS;*/
-        vmaxAdView.setTestDevices(VmaxAdView.TEST_via_ADVID,"<REPLACE WITH YOUR ADVID>");
-
-        vmaxAdView.loadAd();
-
+/** Please Check The Layout activity_main To Find The Implementation*/
     }
 
 
-
-    /** Handle vmaxAdView object for Activity Lifecycle changes */
-
-    @Override
-    protected void onPause() {
-        if (vmaxAdView != null) {
-            /** To Pause Refresh Of The Ad While Activity Isn't in Foreground */
-            vmaxAdView.onPause();
-        }
-
-        super.onPause();
-    }
-    @Override
-    protected void onResume() {
-        if (vmaxAdView != null) {
-            /** To Resume Refresh Of The Ad While Activity Comes Back To Foreground */
-
-            vmaxAdView.onResume();
-        }
-        super.onResume();
-    }
     @Override
     protected void onDestroy() {
-        if (vmaxAdView != null) {
-       /** To Destroy vmaxAdView when Activity Is No Longer Available  */
-            vmaxAdView.onDestroy();
-        }
         super.onDestroy();
+
+    vmaxAdView.onDestroy();
     }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (vmaxAdView != null) {
-
-            /** To Reconfigure vmaxAdview According To Configuration Changes*/
-            vmaxAdView.onConfigurationChanged();
-        }
-
-    }
-
 }
