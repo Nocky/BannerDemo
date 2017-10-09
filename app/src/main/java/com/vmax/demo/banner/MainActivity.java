dd0ad8f8f984266e2f343edf8897095e9e4ef021
prathameshs <prathamesh.s@vmax.com>
2017-10-09 19:07:13 +0530

    3.7.8 update

--- app/src/main/java/com/vmax/demo/banner/MainActivity.java
+++ app/src/main/java/com/vmax/demo/banner/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     VmaxAdView vmaxAdView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vmaxAdView=(VmaxAdView)findViewById(R.id.banner_adview);
+
 
 /** Please Check The Layout activity_main To Find The Implementation*/
     }
